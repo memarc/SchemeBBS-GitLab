fcd89a06789a12146fcca6f5cb40002a4c74964f
Ben Bitdiddle <ben.bitdiddle@protonmail.com>
2020-06-11 15:16:46 +0200

    check if boards were created and default to listen port 8080

--- init.sh
+++ init.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-mit-scheme --args $* < bbs.scm
+if [ ! -d "data/sexp" ] 
+then
+  echo $"You haven't created any board"
+  echo $"run \`./create-boards.sh BOARDNAME...'"
+  exit 1
+elif [ -z "$1" ]
+then
+  mit-scheme --args 8080 < bbs.scm
+else
+  mit-scheme --args $* < bbs.scm
+fi
